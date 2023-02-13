a9e20220060ffd592b3d91c5c7cc092dea9b6aeb
LAWAL <haydelawal@gmail.com>
2023-02-13 08:58:28 +0100

    forage app

--- app/src/main/java/com/example/forage/ui/ForageableListFragment.kt
+++ app/src/main/java/com/example/forage/ui/ForageableListFragment.kt
@@ -21,11 +21,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.example.forage.BaseApplication
 import com.example.forage.R
 import com.example.forage.databinding.FragmentForageableListBinding
 import com.example.forage.ui.adapter.ForageableListAdapter
 import com.example.forage.ui.viewmodel.ForageableViewModel
+import com.example.forage.ui.viewmodel.ForageableViewModelFactory
 
 /**
  * A fragment to view the list of [Forageable]s stored in the database.
@@ -37,13 +40,18 @@ class ForageableListFragment : Fragment() {
     // TODO: Refactor the creation of the view model to take an instance of
     //  ForageableViewModelFactory. The factory should take an instance of the Database retrieved
     //  from BaseApplication
-    private val viewModel: ForageableViewModel by activityViewModels()
-
+    private val viewModel: ForageableViewModel by activityViewModels {
+        ForageableViewModelFactory(
+            (activity?.application as BaseApplication).database.forageableDao()
+        )
+    }
     private var _binding: FragmentForageableListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -65,8 +73,18 @@ class ForageableListFragment : Fragment() {
 
         // TODO: observe the list of forageables from the view model and submit it the adapter
 
+
+
         binding.apply {
             recyclerView.adapter = adapter
+
+
+            viewModel.allForageables.observe(viewLifecycleOwner, Observer { myData ->
+
+                adapter.submitList(myData)
+
+            })
+
             addForageableFab.setOnClickListener {
                 findNavController().navigate(
                     R.id.action_forageableListFragment_to_addForageableFragment
